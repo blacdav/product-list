7fc8d09fc4f190eb96ebce949251149e05976baa
blacdav <davidaniefoik@gmail.com>
2024-12-26 14:28:55 +0100

    updated the way data is fetched from the data.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,40 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import cart_icon from './assets/icon-add-to-cart.svg'
 import decrease from './assets/icon-decrement-quantity.svg'
 import increase from './assets/icon-increment-quantity.svg'
 import Cart from "./components/Cart";
 import { useCart } from "./context/CartContext";
 import ConfirmOrder from "./modals/ConfirmOrder";
+import data from "../public/data.json"
 
-interface Image {
-  thumbnail: string,
-  mobile: string,
-  tablet: string,
-  desktop: string,
-}
+// interface Image {
+//   thumbnail: string,
+//   mobile: string,
+//   tablet: string,
+//   desktop: string,
+// }
 
-interface Dessert {
-  name: string,
-  price: number,
-  image: Image,
-  category: string,
-  quantity: number,
-}
+// interface Dessert {
+//   name: string,
+//   price: number,
+//   image: Image,
+//   category: string,
+//   quantity: number,
+// }
 const App: React.FC = () => {
   const [isConfirmed, setIsConfirmed] = useState<boolean>(false);
   const { isInCart, addItem, increaseQuantity, decreaseQuantity } = useCart()
-  const [data, setData] = useState<Dessert[]>([]);
-  
-  useEffect(() => {
-    const getData = async () => {
-      const response = await fetch('https://blacdav-product-list.netlify.app//data.json');
-      const data = await response.json();
-      setData(data);
-    }
+  // const [data, setData] = useState<Dessert[]>([]);
 
-    getData();
-  }, []);
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const response = await fetch('https://blacdav-product-list.netlify.app//data.json');
+  //     const data = await response.json();
+  //     setData(data);
+  //   }
+
+  //   getData();
+  // }, []);
 
   return (
     <>
@@ -41,7 +42,7 @@ const App: React.FC = () => {
         <section className="col-span-2">
           <h1 className='font-bold text-5xl'>Desserts</h1>
           <div className="mt-5 grid md:grid-cols-3 gap-y-6 gap-x-4">
-            {data.map((dessert: Dessert, index: number) => (
+            {data.map((dessert, index) => (
               <div key={index}>
                 <div className="grid">
                   <img src={dessert.image.desktop} alt="item" className={`${ isInCart(dessert.name) ? 'border-2' : ''} h-56 md:h-full w-full rounded-md border-primary`} />
